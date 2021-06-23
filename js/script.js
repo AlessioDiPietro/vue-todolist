6520cdb8e7fa9242fdabe4649aca10f496410094
AlessioDPT <alessio.recos@gmail.com>
2021-06-23 15:23:41 +0200

    ADD- step 1 (stampa array)

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
-console.log("amici");
+// Descrizione: Rifare l'esercizio della to do list come fatto assieme in classe:
+// - stampare in pagina un item per ogni elemento contenuto in un array
+// - ogni item ha una "x" associata: cliccando su di essa, l'item viene rimosso dalla lista
+// - predisporre un input per aggiungere un nuovo item alla lista: digitando il tasto invio oppure ciccando su un pulsante, il testo digitato viene aggiunto alla lista
+
+const app = new Vue ({
+
+    el : "#app",
+
+    data: {
+        todolist: [
+            "Bere una pozione che ti fa rimpicciolire",
+            "Mangiare una torta che ti fa ingrandire",
+            "parlare con gli animali",
+            "Vedere i gatti che evaporano",
+            "Esplorare il Paese delle Meraviglie",
+            "Uccidere il Ciciarampa!"
+        ]
+    }
+});
